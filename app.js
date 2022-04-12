c3a12d7c75eecc6a315bdb8d9e471998302fd626
Eva892 <https://github.com/Eva892/NodeJS_CRUD.git>
2022-04-13 00:13:16 +0600

    Add Server

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const app = express();
 
 const studentRoutes = require('./api/routes/students');
 
-app.use('/students',studentRoutes);
-
-
+app.use('/students',studentRoutes)
 
 module.exports = app;
